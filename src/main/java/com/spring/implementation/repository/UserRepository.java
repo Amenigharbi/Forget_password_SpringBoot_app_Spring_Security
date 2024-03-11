9b115bb47c7eb964e275ace73893392ab0657ca9
Ameni <103783268+Amenigharbi@users.noreply.github.com>
2024-03-11 15:40:01 +0100

    Update UserRepository.java

--- src/main/java/com/spring/implementation/repository/UserRepository.java
+++ src/main/java/com/spring/implementation/repository/UserRepository.java
@@ -1,10 +1,8 @@
 package com.spring.implementation.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.spring.implementation.model.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
-
+public interface UserRepository extends JpaRepository<User, Integer> 
+{
 	User findByEmail(String email);
 }
